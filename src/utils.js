7eeb9762dd8b24f6c264e827f4d2ac4ce7b3cd9c
stas <stas.masorov@mail.ru>
2022-12-13 11:26:31 +0500

    README.md and package.json fix

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 const getRandom = (max = 100, min = 1) => {
-  const randomnum = Math.round(Math.random() * (max - min)) + 1;
+  const randomnum = Math.floor(Math.random() * (max - min)) + 1;
   return randomnum;
 };
 
