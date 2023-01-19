ce293abfae703306f99e7bfaca478a39fcd33928
stas <stas.masorov@mail.ru>
2023-01-19 11:49:52 +0500

    final fix-9

--- src/games/calc.js
+++ src/games/calc.js
@@ -3,27 +3,24 @@ import engine from '../index.js';
 
 const rules = 'What is the result of the expression?';
 
-const RandomOperator = () => {
+const randomOperator = () => {
   const operators = ['+', '-', '*'];
   return operators[getRandom(0, operators.length - 1)];
 };
 
-const calculate = (numb1, numb2, operator) => {
-  let calcAnswer = '';
-  if (operator === '+') {
-    calcAnswer += String(numb1 + numb2);
-  } else if (operator === '-') {
-    calcAnswer += String(numb1 - numb2);
-  } else if (operator === '*') {
-    calcAnswer += String(numb1 * numb2);
+const calculate = (num1, num2, operator) => {
+  switch (operator) {
+    case '+': return String(num1 + num2);
+    case '-': return String(num1 - num2);
+    case '*': return String(num1 * num2);
+    default: throw new Error(`Operator ${operator} - is invalid`);
   }
-  return calcAnswer;
 };
 
 const generateRound = () => {
   const num1 = getRandom(1, 50);
   const num2 = getRandom(1, 50);
-  const operator = RandomOperator();
+  const operator = randomOperator();
   const question = `Question: ${num1} ${operator} ${num2}`;
   const answer = calculate(num1, num2, operator);
   return [question, answer];
